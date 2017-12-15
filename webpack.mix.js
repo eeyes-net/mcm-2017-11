270f2604b577553f2a2918099eb71bc447e1dbf8
Ganlv <ganlvtech@qq.com>
2017-12-16 02:40:13 +0800

    删除app.js和app.scss

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,6 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
 mix
     .js('resources/assets/js/admin.js', 'public/js')
     .sass('resources/assets/sass/admin.scss', 'public/css')
